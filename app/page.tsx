405e49d22ef6393f0cfeeba50afa33aa2e61ddb9
sanketm-hub <sanketm@salttechno.com>
2025-09-16 18:11:49 +0530

    code updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,46 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="lg:max-w-[1080px] xl:max-w-[1140px] 2xl:max-w-[1392px] mx-auto text-center px-5 py-10 md:py-28 md:px-6 lg:py-28
+      2xl:py-40 xl:py-0">
+        <h1 className="font-bold text-3xl sm:text-4xl md:text-[50px]/[60px] xl:text-[80px]/[90px] 2xl:text-[100px]/[120px] 3xl:text-[128px]/[140px]">
+          THE AWARD-WINNING
+          <br /> UX DEPARTMENT
+        </h1>
+        <p className="font-bold text-lg leading-[35px] md:text-xl md:leading-[40px] lg:text-[42] lg:leading-[60px] 2xl:text-[56px] 2xl:leading-[76px]">
+          For<span className="text-[#00CE70]"> Healthcare technology</span> Teams
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="lg:max-w-[1080px] xl:max-w-[1158px] 2xl:max-w-[1392px] mx-auto text-center px-5 pb-10  md:py-10 md:px-6  xl:py-16 xl:px-0">
+        <h2 className="font-normal text-base mb-4 md:text-lg md:mb-6 xl:text-[34px] xl:leading-[48px] xl:mb-8">
+          We’re a <span className="font-semibold">UX design agency</span> with
+            1,500+ healthcare projects under our belt.
+            <br className="hidden 2xl:inline"/>We've helped millions of
+            patients, clinicians and medical providers experience
+             <br className="hidden 2xl:inline"/> simpler, smarter digital care.
+        </h2>
+        <div className="lg:max-w-[1080px] xl:max-w-[1140px] 2xl:max-w-[1392px] mx-auto">
+          <div className="flex flexx-wrap items-center justify-center gap-x-4 gap-y-6 2xl:gap-12 xl:justify-between xl:mx-0">
+            <div>
+              <Image
+                src="/images/forbes.png"
+                alt="Forbes Logo"
+                width={155.7}
+                height={19.43}
+                className="w-28 xl:w-[151px]"
+                />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <h2 className=" font-normal text-base md:text-lg xl:text-[34px] xl:leading-[48px] mt-6 lg:mt-10 xl:mt-14">
+            Need support with user research, design, usability testing, or UX
+            strategy? Let’s talk.
+          </h2>
+      </div>
+    </>
   );
 }
